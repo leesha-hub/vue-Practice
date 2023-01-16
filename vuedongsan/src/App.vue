05ef4cd683db8a75d91ffbf8f2e27a63d680091a
leeshahub <leesha1223@naver.com>
2023-01-16 22:54:41 +0900

    lesson1 데이터바인딩

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  안녕하세요asdasdasd
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    원룸샵
+    <h4>{{products.at(0)}}</h4>
+    <!-- <h4 class="red" :style="스타일">xx 원룸</h4> -->
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{products.at(1)}}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{products.at(2)}}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      price1 : 60,
+      price2 : 70,
+      //스타일 : 'color:blue',
+      products : ['역삼동원룸','천호동원룸','마포구원룸']
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
